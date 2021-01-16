f9540e13f4424fd337a33812ef4a9faabe8886cb
Dor <doramid1590@gmail.com>
2021-01-16 19:11:00 +0200

    works???

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const fs = require('fs');
+const fs = require('fs	');
 const app = express ();
 app.all('*',(req,res)=> {
 console.log(req);
@@ -9,4 +9,4 @@ var s = fs.createReadStream('./gif.gif');
         s.pipe(res);
     });
 });
-app.listen(8080, () => console.log('start'));
+app.listen(process.env.PORT || 5000, () => console.log('start'));
