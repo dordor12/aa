6cb856a015b36f097b33640f4b03777ca6ddd30f
Dor <doramid1590@gmail.com>
2021-01-16 19:02:26 +0200

    now works?

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const fs = require('fs');
 const app = express ();
 app.all('*',(req,res)=> {
 console.log(req);
+var s = fs.createReadStream('./gif.gif');
+    s.on('open', function () {
+        res.setHeader('Content-Type', 'image/gif');
+        s.pipe(res);
+    });
 });
-app.listen(80, () => console.log('start'));
+app.listen(8080, () => console.log('start'));
